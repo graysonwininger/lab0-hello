a2cae31ba75c704e12b074918d90be496ab3ac2b
Grayson Wininger <winingerg1@etsu.edu>
2026-09-09 12:00:23 -0400

    Add header comment and introduction lines

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+/*
+* Name: Grayson Wininger
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Grayson Wininger.");
+Console.WriteLine("This is my first program at ETSU.");
