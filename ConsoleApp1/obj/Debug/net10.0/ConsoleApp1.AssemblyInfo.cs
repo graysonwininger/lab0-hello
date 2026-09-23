5164de63a593441c37801992dbe71f53f0fdba2d
Grayson Wininger <winingerg1@etsu.edu>
2026-09-23 11:55:22 -0400

    Lab02

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5dd02fd90cfce1f68ea9eb0e4f5edae1e3cd98e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b2e8e925542de6492e335d93fd15bf6d6e7d82")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
